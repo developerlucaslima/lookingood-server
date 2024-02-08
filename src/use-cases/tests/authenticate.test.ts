b1bda17dc1fc45732eaa2b029cc4be83b8d6040b
developerlucaslima <developerlucaslima@gmail.com>
2024-02-08 18:23:10 -0300

    ♻️ refactor: imports and eslint config

--- src/use-cases/tests/authenticate.test.ts
+++ src/use-cases/tests/authenticate.test.ts
@@ -1,8 +1,8 @@
 import { expect, describe, it, beforeEach } from 'vitest'
 import { hash } from 'bcryptjs'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
-import { AuthenticateUseCase } from './authenticate'
-import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+import { InvalidCredentialsError } from '../errors/invalid-credentials-error'
+import { AuthenticateUseCase } from '../factories/authenticate'
 
 let usersRepository: InMemoryUsersRepository
 let sut: AuthenticateUseCase
