63185f5f402dd36d860c1b0b62cc754697140025
developerlucaslima <developerlucaslima@gmail.com>
2024-02-15 14:46:42 -0300

    🗃️ raw: add some columns to establishments

--- prisma/migrations/20240215173510_first_migration/migration.sql
+++ prisma/migrations/20240215173510_first_migration/migration.sql
@@ -2,6 +2,9 @@
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
@@ -10,8 +13,11 @@ CREATE TABLE "users" (
 CREATE TABLE "establishments" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "address" TEXT NOT NULL,
-    "imageUrl" TEXT NOT NULL,
+    "description" TEXT,
+    "phone" TEXT,
+    "imageUrl" TEXT,
+    "latitude" DECIMAL(65,30) NOT NULL,
+    "longitude" DECIMAL(65,30) NOT NULL,
 
     CONSTRAINT "establishments_pkey" PRIMARY KEY ("id")
 );
@@ -38,6 +44,9 @@ CREATE TABLE "bookings" (
 
     CONSTRAINT "bookings_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- AddForeignKey
 ALTER TABLE "services" ADD CONSTRAINT "services_establishmentId_fkey" FOREIGN KEY ("establishmentId") REFERENCES "establishments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
