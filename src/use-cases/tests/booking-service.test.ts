144e792e8337ae24b4f11d946eab1ec11ae6cf87
developerlucaslima <developerlucaslima@gmail.com>
2024-02-16 16:33:07 -0300

    🧪 test: booking service test

---
+++ src/use-cases/tests/booking-service.test.ts
@@ -0,0 +1,35 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryBookingsRepository } from '@/repositories/in-memory/in-memory-bookings-repository'
+import { BookingServiceUseCase } from '../factories/booking-service'
+import { InMemoryEstablishmentsRepository } from '@/repositories/in-memory/in-memory-establishments-repository'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+
+let establishmentsRepository: InMemoryEstablishmentsRepository
+let bookingsRepository: InMemoryBookingsRepository
+let usersRepository: InMemoryUsersRepository
+let sut: BookingServiceUseCase
+
+describe('Create Establishment Use Case', () => {
+  beforeEach(() => {
+    establishmentsRepository = new InMemoryEstablishmentsRepository()
+    bookingsRepository = new InMemoryBookingsRepository()
+    usersRepository = new InMemoryUsersRepository()
+    sut = new BookingServiceUseCase(
+      establishmentsRepository,
+      bookingsRepository,
+      usersRepository,
+    )
+  })
+
+  it('should to booking a service', async () => {
+    const { booking } = await sut.execute({
+      date: new Date(2022, 1, 1),
+      status: 'Pendente',
+      userId: 'user-01',
+      serviceId: 'service-01',
+      establishmentId: 'establishment-01',
+    })
+
+    expect(booking.id).toEqual(expect.any(String))
+  })
+})
