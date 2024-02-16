ee95843ef2ab7e042c10b6d55483175ae76c16d7
developerlucaslima <developerlucaslima@gmail.com>
2024-02-15 22:13:29 -0300

    ✨ feat: service use case

---
+++ src/use-cases/factories/create-service.ts
@@ -0,0 +1,50 @@
+import { EstablishmentsRepository } from '@/repositories/establishment-repository'
+import { ServicesRepository } from '@/repositories/services-repository'
+import { Service } from '@prisma/client'
+import { ResourceNotFoundError } from '../errors/resource-not-found-error'
+
+interface CreateServiceUseCaseRequest {
+  name: string
+  price: number
+  description: string | null
+  imageUrl: string | null
+  establishmentId: string
+}
+
+interface CreateServiceUseCaseResponse {
+  service: Service
+}
+
+export class CreateServiceUseCase {
+  constructor(
+    private establishmentRepository: EstablishmentsRepository,
+    private servicesRepository: ServicesRepository,
+  ) {}
+
+  async execute({
+    name,
+    price,
+    description,
+    imageUrl,
+    establishmentId,
+  }: CreateServiceUseCaseRequest): Promise<CreateServiceUseCaseResponse> {
+    const establishment =
+      await this.establishmentRepository.findById(establishmentId)
+
+    if (!establishment) {
+      throw new ResourceNotFoundError()
+    }
+
+    const service = await this.servicesRepository.create({
+      name,
+      price,
+      description,
+      imageUrl,
+      establishmentId,
+    })
+
+    return {
+      service,
+    }
+  }
+}
