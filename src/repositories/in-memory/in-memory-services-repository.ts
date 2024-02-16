d8220d6f0c07bf34581e804f2d5fa2164ed141dd
developerlucaslima <developerlucaslima@gmail.com>
2024-02-16 16:00:21 -0300

    🚚 chore: files renamed

--- src/repositories/in-memory/in-memory-services-repository.ts
+++ src/repositories/in-memory/in-memory-services-repository.ts
@@ -6,25 +6,6 @@ import { ServicesRepository } from '../services-repository'
 export class InMemoryServicesRepository implements ServicesRepository {
   public items: Service[] = []
 
-  // async findByUserIdOnDate(userId: string, date: Date) {
-  //   const startOfTheDay = dayjs(date).startOf('date')
-  //   const endOfTheDay = dayjs(date).endOf('date')
-
-  //   const serviceOnSameDate = this.items.find((service) => {
-  //     const serviceDate = dayjs(service.created_at)
-  //     const isOnSameDate =
-  //       serviceDate.isAfter(startOfTheDay) && serviceDate.isBefore(endOfTheDay)
-
-  //     return service.user_id === userId && isOnSameDate
-  //   })
-
-  //   if (!serviceOnSameDate) {
-  //     return null
-  //   }
-
-  //   return serviceOnSameDate
-  // }
-
   async create(data: Prisma.ServiceUncheckedCreateInput) {
     const service = {
       id: randomUUID(),
