7b5a7ed0e772a3d0e8820b2867c33e34f45dcfb4
developerlucaslima <developerlucaslima@gmail.com>
2024-02-20 15:10:59 -0300

    🔜feat: todo new services methods

--- src/repositories/services-repository.ts
+++ src/repositories/services-repository.ts
@@ -2,4 +2,6 @@ import { Service, Prisma } from '@prisma/client'
 
 export interface ServicesRepository {
   create(data: Prisma.ServiceUncheckedCreateInput): Promise<Service>
+  // findAllByGender
+  // findAllByEstablishmentId
 }
