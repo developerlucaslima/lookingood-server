656e17ff881f88937d6507c7c100828d7485c4df
developerlucaslima <developerlucaslima@gmail.com>
2024-02-25 13:45:06 -0300

    🔜✨ feat: booking service and new ideas

--- src/use-cases/factories/booking-service.ts
+++ src/use-cases/factories/booking-service.ts
@@ -1,15 +1,17 @@
-import { EstablishmentsRepository } from '@/repositories/establishments-repository'
 import { UsersRepository } from '@/repositories/users-repository'
 import { Booking } from '@prisma/client'
 import { ResourceNotFoundError } from '../errors/resource-not-found-error'
-import { BookingRepository } from '@/repositories/bookings-repository'
+import { BookingsRepository } from '@/repositories/bookings-repository'
+import { ServicesRepository } from '@/repositories/services-repository'
+import { EstablishmentsRepository } from '@/repositories/establishments-repository'
+import { ProfessionalsRepository } from '@/repositories/professionals-repository'
 
 interface BookingServiceUseCaseRequest {
   date: Date
   status: string
   userId: string
   serviceId: string
-  establishmentId: string
+  professionalId: string
 }
 
 interface BookingServicesUseCaseResponse {
@@ -19,7 +21,9 @@ interface BookingServicesUseCaseResponse {
 export class BookingServiceUseCase {
   constructor(
     private establishmentsRepository: EstablishmentsRepository,
-    private bookingRepository: BookingRepository,
+    private professionalsRepository: ProfessionalsRepository,
+    private servicesRepository: ServicesRepository,
+    private bookingsRepository: BookingsRepository,
     private usersRepository: UsersRepository,
   ) {}
 
@@ -28,27 +32,51 @@ export class BookingServiceUseCase {
     status,
     userId,
     serviceId,
-    establishmentId,
+    professionalId,
   }: BookingServiceUseCaseRequest): Promise<BookingServicesUseCaseResponse> {
-    const establishment =
-      await this.establishmentsRepository.findById(establishmentId)
+    // TODO: service and professional from the same establishment?
+    // TODO: this establishment exits by id?
+    // TODO: fazer esses testes
+    // TODO: status, definir 3 status
+    // TODO: nao deve ser possivel agendar com o mesmo profissional no mesmo horario
 
+    const professional = await this.professionalsRepository.findById(serviceId)
+    if (!professional) {
+      throw new ResourceNotFoundError() // TODO: create specific error
+    }
+
+    const service = await this.servicesRepository.findById(serviceId)
+    if (!service) {
+      throw new ResourceNotFoundError() // TODO: create specific error
+    }
+
+    const user = await this.usersRepository.findById(userId)
+    if (!user) {
+      throw new ResourceNotFoundError() // TODO: create specific error
+    }
+
+    if (service.establishmentId !== professional.establishmentId) {
+      throw new ResourceNotFoundError() // TODO: create specific error
+    }
+
+    const establishment = await this.establishmentsRepository.findById(
+      service.establishmentId,
+    )
     if (!establishment) {
       throw new ResourceNotFoundError()
     }
 
-    const user = await this.usersRepository.findById(userId)
-
-    if (!user) {
-      throw new ResourceNotFoundError()
+    // TODO: to think status names, to create only booked, to update can be anothers
+    if (!['Booked', 'Confirmed', 'Check-Out', 'No-Show'].includes(status)) {
+      throw new ResourceNotFoundError() // TODO: create specific error
     }
 
-    const booking = await this.bookingRepository.create({
+    const booking = await this.bookingsRepository.create({
       date,
       status,
       userId,
       serviceId,
-      establishmentId,
+      professionalId,
     })
 
     return {
