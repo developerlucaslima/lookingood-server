8a905831b4b924c713f58ed8e6b250af6c7d0845
developerlucaslima <developerlucaslima@gmail.com>
2024-04-05 17:56:52 -0300

    ♻️ refactor: bookings repository interface

--- src/repositories/bookings-repository.ts
+++ src/repositories/bookings-repository.ts
@@ -2,7 +2,8 @@ import { Booking, Prisma } from '@prisma/client'
 
 export interface BookingsRepository {
   create(data: Prisma.BookingUncheckedCreateInput): Promise<Booking>
-  update(booking: Booking): Promise<Booking>
+  findById(id: string): Promise<Booking | null>
+  update(data: Prisma.BookingUncheckedCreateInput): Promise<Booking>
   findManyByUserId(userId: string): Promise<Booking[] | null>
   isBookingConflict(
     professionalId: string,
@@ -10,7 +11,6 @@ export interface BookingsRepository {
     endTime: Date,
   ): Promise<boolean>
   findManyByStatus(status: string): Promise<Booking[] | null>
-  findById(id: string): Promise<Booking | null>
 }
 
 // findByProfessionalAndDate
