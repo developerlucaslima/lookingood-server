228f85a9dd2649a20bf26f74d99e9ce607177493
developerlucaslima <developerlucaslima@gmail.com>
2024-04-05 17:54:27 -0300

    ✨ feat: prisma professional repository

---
+++ src/repositories/prisma/prisma-professionals-repository.ts
@@ -0,0 +1,24 @@
+import { prisma } from '@/lib/prisma'
+import { Prisma } from '@prisma/client'
+
+import { ProfessionalsRepository } from '../professionals-repository'
+
+export class PrismaProfessionalsRepository implements ProfessionalsRepository {
+  async create(data: Prisma.ProfessionalUncheckedCreateInput) {
+    const professional = await prisma.professional.create({
+      data,
+    })
+
+    return professional
+  }
+
+  async findById(id: string) {
+    const professional = await prisma.professional.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return professional
+  }
+}
