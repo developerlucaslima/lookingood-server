bea1032a6dd2fd9b0d97d7bd523cd362fe0d8772
developerlucaslima <developerlucaslima@gmail.com>
2024-04-16 15:35:04 -0300

    ♻️ refactor: jwt expires in 10 min to refresh

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import fastify from 'fastify'
 import { appRoutes } from '@/http/routes'
 import { ZodError } from 'zod'
@@ -8,7 +9,12 @@ export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  sign: {
+    expiresIn: '10m',
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(appRoutes)
 
