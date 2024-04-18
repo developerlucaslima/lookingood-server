38f3a5f13330b51159dc9924d90733e178eee6d5
developerlucaslima <developerlucaslima@gmail.com>
2024-04-18 00:13:25 -0300

    ✨ feat: add establishment id to confirm booked service use case request

--- src/use-cases/confirm-booked-service.ts
+++ src/use-cases/confirm-booked-service.ts
@@ -1,9 +1,12 @@
 import { Booking } from '@prisma/client'
 import { BookingsRepository } from '@/repositories/bookings-repository'
 import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
+import { EstablishmentNotFoundError } from './errors/establishment-not-found-error'
+import { EstablishmentsRepository } from '@/repositories/establishments-repository'
 
 interface ConfirmBookedServiceUseCaseRequest {
   bookingId: string
+  establishmentId: string
 }
 
 interface ConfirmBookedServicesUseCaseResponse {
@@ -11,15 +14,25 @@ interface ConfirmBookedServicesUseCaseResponse {
 }
 
 export class ConfirmBookedServiceUseCase {
-  constructor(private bookingsRepository: BookingsRepository) {}
+  constructor(
+    private bookingsRepository: BookingsRepository,
+    private establishmentsRepository: EstablishmentsRepository,
+  ) {}
 
   async execute({
     bookingId,
+    establishmentId,
   }: ConfirmBookedServiceUseCaseRequest): Promise<ConfirmBookedServicesUseCaseResponse> {
     const booking = await this.bookingsRepository.findById(bookingId)
 
     if (!booking) {
       throw new ResourceNotFoundError()
+    }
+
+    const establishment =
+      await this.establishmentsRepository.findById(establishmentId)
+    if (!establishment || establishment.id !== booking.establishmentId) {
+      throw new EstablishmentNotFoundError()
     }
 
     booking.status = `Confirmed`
