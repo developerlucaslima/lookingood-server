b4af3239a0eff24b403c0043c7d172fc65aad973
developerlucaslima <developerlucaslima@gmail.com>
2024-04-24 22:39:00 -0300

    🗑️ remove: files renamed or unused

--- src/utils/convert-time-to-minutes.ts
+++
@@ -1,7 +0,0 @@
-export function convertTimeToMinutes(time: string): number {
-  const [hourStr, minuteStr] = time.split(':')
-  const hourInt = parseInt(hourStr, 10) * 60
-  const minutesInt = parseInt(minuteStr, 10)
-
-  return hourInt + minutesInt
-}
