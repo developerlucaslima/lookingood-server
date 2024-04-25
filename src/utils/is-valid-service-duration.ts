d981bb8059fa4649b7c2be9a03c3784556881811
developerlucaslima <developerlucaslima@gmail.com>
2024-04-24 22:42:42 -0300

    ✨ feat: util to verify if service has a valid duration

---
+++ src/utils/is-valid-service-duration.ts
@@ -0,0 +1,11 @@
+export function isValidServiceDuration(durationMinutes: number): boolean {
+  if (!Number.isInteger(durationMinutes) || durationMinutes <= 0) {
+    return false
+  }
+
+  if (durationMinutes % 15 !== 0) {
+    return false
+  }
+
+  return true
+}
