04d4143e0ca3641c83526130897b69f6f5873cd5
developerlucaslima <developerlucaslima@gmail.com>
2024-04-25 17:31:29 -0300

    ✨ feat: change to map to store data

---
+++ src/repositories/in-memory/in-memory-professionals-schedule-repository.ts
@@ -0,0 +1,50 @@
+import { Prisma, ProfessionalSchedule } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import { ProfessionalsSchedulesRepository } from '../professionals-schedules-repository'
+
+export class InMemoryProfessionalsScheduleRepository
+  implements ProfessionalsSchedulesRepository
+{
+  public items: Map<ProfessionalSchedule['id'], ProfessionalSchedule> =
+    new Map()
+
+  async create(data: Prisma.ProfessionalScheduleUncheckedCreateInput) {
+    const professionalSchedule: ProfessionalSchedule = {
+      id: randomUUID(),
+      startTime: data.startTime,
+      minutesWorking: data.minutesWorking,
+      breakTime: data.breakTime ?? null,
+      minutesBreak: data.minutesBreak ?? null,
+      weekDay: data.weekDay,
+      professionalId: data.professionalId,
+    }
+
+    this.items.set(professionalSchedule.id, professionalSchedule)
+
+    return professionalSchedule
+  }
+
+  async findManyByProfessionalId(professionalId: string) {
+    const manySchedules: ProfessionalSchedule[] = []
+    for (const schedule of this.items.values()) {
+      if (schedule.professionalId === professionalId) {
+        manySchedules.push(schedule)
+      }
+    }
+    return manySchedules
+  }
+
+  async findByProfessionalIdAndWeekDay(
+    professionalId: string,
+    weekDay: string,
+  ) {
+    const schedules = Array.from(this.items.values()).flat()
+    return (
+      schedules.find(
+        (schedule) =>
+          schedule.professionalId === professionalId &&
+          schedule.weekDay === weekDay,
+      ) || null
+    )
+  }
+}
