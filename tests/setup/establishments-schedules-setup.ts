de250bfb75abad3bf9573aaa4e2f97ff1174da51
developerlucaslima <developerlucaslima@gmail.com>
2024-04-27 17:08:57 -0300

    ✨ feat: establishment schedule use case refactor and improved comments

--- tests/setup/establishments-schedules-setup.ts
+++ tests/setup/establishments-schedules-setup.ts
@@ -1,84 +1,63 @@
-import { InMemoryEstablishmentsScheduleRepository } from '@/repositories/in-memory/in-memory-establishments-schedule-repository'
+import { InMemoryEstablishmentsSchedulesRepository } from '@/repositories/in-memory/in-memory-establishments-schedules-repository'
+import { WeekDay } from '@prisma/client'
 
 export const establishmentsSchedulesSetup = async (
-  schedulesRepository: InMemoryEstablishmentsScheduleRepository,
+  schedulesRepository: InMemoryEstablishmentsSchedulesRepository,
 ) => {
   const barberId = 'Establishment-01'
+  const spaId = 'Establishment-02'
 
-  const mondayScheduleId = 'Schedule-Monday'
-  schedulesRepository.items.set(mondayScheduleId, {
-    id: mondayScheduleId,
-    startTime: '07:00',
-    minutesWorking: 720,
-    breakTime: '12:00',
-    minutesBreak: 90,
-    weekDay: 'MONDAY',
-    establishmentId: barberId,
+  const startTimeBarber = '07:00'
+  const minutesWorkingBarber = 720
+  const breakTimeBarber = '12:00'
+  const minutesBreakBarber = 90
+
+  const startTimeSpa = '08:00'
+  const minutesWorkingSpa = 600
+  const breakTimeSpa = '13:00'
+  const minutesBreakSpa = 60
+
+  const barberSchedules = [
+    { weekDay: 'MONDAY' as WeekDay, id: 'Schedule-Monday-Barber' },
+    { weekDay: 'TUESDAY' as WeekDay, id: 'Schedule-Tuesday-Barber' },
+    { weekDay: 'WEDNESDAY' as WeekDay, id: 'Schedule-Wednesday-Barber' },
+    { weekDay: 'THURSDAY' as WeekDay, id: 'Schedule-Thursday-Barber' },
+    { weekDay: 'FRIDAY' as WeekDay, id: 'Schedule-Friday-Barber' },
+    { weekDay: 'SATURDAY' as WeekDay, id: 'Schedule-Saturday-Barber' },
+    // { weekDay: 'SUNDAY' as WeekDay, id: 'Schedule-Sunday-Barber' },
+  ]
+
+  const spaSchedules = [
+    { weekDay: 'MONDAY' as WeekDay, id: 'Schedule-Monday-Spa' },
+    { weekDay: 'TUESDAY' as WeekDay, id: 'Schedule-Tuesday-Spa' },
+    { weekDay: 'WEDNESDAY' as WeekDay, id: 'Schedule-Wednesday-Spa' },
+    { weekDay: 'THURSDAY' as WeekDay, id: 'Schedule-Thursday-Spa' },
+    { weekDay: 'FRIDAY' as WeekDay, id: 'Schedule-Friday-Spa' },
+    { weekDay: 'SATURDAY' as WeekDay, id: 'Schedule-Saturday-Spa' },
+    { weekDay: 'SUNDAY' as WeekDay, id: 'Schedule-Sunday-Spa' },
+  ]
+
+  barberSchedules.forEach(({ weekDay, id }) => {
+    schedulesRepository.items.set(id, {
+      id,
+      startTime: startTimeBarber,
+      minutesWorking: minutesWorkingBarber,
+      breakTime: breakTimeBarber,
+      minutesBreak: minutesBreakBarber,
+      weekDay,
+      establishmentId: barberId,
+    })
   })
 
-  const tuesdayScheduleId = 'Schedule-Tuesday'
-  schedulesRepository.items.set(tuesdayScheduleId, {
-    id: tuesdayScheduleId,
-    startTime: '07:00',
-    minutesWorking: 720,
-    breakTime: '12:00',
-    minutesBreak: 90,
-    weekDay: 'TUESDAY',
-    establishmentId: barberId,
-  })
-
-  const wednesdayScheduleId = 'Schedule-Wednesday'
-  schedulesRepository.items.set(wednesdayScheduleId, {
-    id: wednesdayScheduleId,
-    startTime: '07:00',
-    minutesWorking: 720,
-    breakTime: '12:00',
-    minutesBreak: 90,
-    weekDay: 'WEDNESDAY',
-    establishmentId: barberId,
-  })
-
-  const thursdayScheduleId = 'Schedule-Thursday'
-  schedulesRepository.items.set(thursdayScheduleId, {
-    id: thursdayScheduleId,
-    startTime: '07:00',
-    minutesWorking: 720,
-    breakTime: '12:00',
-    minutesBreak: 90,
-    weekDay: 'THURSDAY',
-    establishmentId: barberId,
-  })
-
-  const fridayScheduleId = 'Schedule-Friday'
-  schedulesRepository.items.set(fridayScheduleId, {
-    id: fridayScheduleId,
-    startTime: '07:00',
-    minutesWorking: 720,
-    breakTime: '12:00',
-    minutesBreak: 90,
-    weekDay: 'FRIDAY',
-    establishmentId: barberId,
-  })
-
-  const saturdayScheduleId = 'Schedule-Saturday'
-  schedulesRepository.items.set(saturdayScheduleId, {
-    id: saturdayScheduleId,
-    startTime: '07:00',
-    minutesWorking: 540,
-    breakTime: '12:00',
-    minutesBreak: 90,
-    weekDay: 'SATURDAY',
-    establishmentId: barberId,
-  })
-
-  const sundayScheduleId = 'Schedule-Sunday'
-  schedulesRepository.items.set(sundayScheduleId, {
-    id: sundayScheduleId,
-    startTime: '07:00',
-    minutesWorking: 540,
-    breakTime: '12:00',
-    minutesBreak: 90,
-    weekDay: 'SUNDAY',
-    establishmentId: barberId,
+  spaSchedules.forEach(({ weekDay, id }) => {
+    schedulesRepository.items.set(id, {
+      id,
+      startTime: startTimeSpa,
+      minutesWorking: minutesWorkingSpa,
+      breakTime: breakTimeSpa,
+      minutesBreak: minutesBreakSpa,
+      weekDay,
+      establishmentId: spaId,
+    })
   })
 }
