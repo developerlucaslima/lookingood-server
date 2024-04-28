2dcaa4a75fb42875a4dfd22cccc27531a9455b1d
developerlucaslima <developerlucaslima@gmail.com>
2024-04-28 17:19:42 -0300

    🧪 test: semantics improved and setup deleted

--- tests/Integration/establishment-register.test.ts
+++ tests/Integration/establishment-register.test.ts
@@ -1,25 +1,38 @@
 import { expect, describe, it, beforeEach } from 'vitest'
-import { compare } from 'bcryptjs'
+import { compare, hash } from 'bcryptjs'
 import { InMemoryEstablishmentsRepository } from '@/repositories/in-memory/in-memory-establishments-repository'
 import { EstablishmentRegisterUseCase } from '@/use-cases/establishment-register'
 import { EmailNotAvailableException } from '@/use-cases/errors/409-email-not-available-exception.ts'
-import { establishmentsSetup } from 'tests/setup/establishments-setup'
+import { Decimal } from '@prisma/client/runtime/library'
 
 let establishmentsRepository: InMemoryEstablishmentsRepository
 let sut: EstablishmentRegisterUseCase
 
 describe('Establishment Register Use Case', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     establishmentsRepository = new InMemoryEstablishmentsRepository()
     sut = new EstablishmentRegisterUseCase(establishmentsRepository)
 
-    establishmentsSetup(establishmentsRepository)
+    const establishmentId = 'Establishment-01'
+    establishmentsRepository.items.set(establishmentId, {
+      id: establishmentId,
+      name: 'Registered Establishment',
+      description: 'Registered establishment...',
+      phone: '55 555-5555',
+      imageUrl: 'image.url',
+      email: 'registered_establishment@example.com',
+      passwordHash: await hash('123456', 6),
+      createdAt: new Date(),
+      latitude: new Decimal(-27.2092052),
+      longitude: new Decimal(-49.6401091),
+      role: 'ESTABLISHMENT',
+    })
   })
 
   it('should allow to register a establishment', async () => {
     const { establishment } = await sut.execute({
       name: 'Salon',
-      description: 'Best salon of the city',
+      description: 'Salon under registration...',
       phone: '55 555-5555',
       imageUrl: 'image.url',
       email: 'salon@example.com',
@@ -34,7 +47,7 @@ describe('Establishment Register Use Case', () => {
   it('should hash establishment password upon registration', async () => {
     const { establishment } = await sut.execute({
       name: 'Salon',
-      description: 'Best salon of the city',
+      description: 'Salon under registration...',
       phone: '55 555-5555',
       imageUrl: 'image.url',
       email: 'salon@example.com',
@@ -55,10 +68,10 @@ describe('Establishment Register Use Case', () => {
     await expect(() =>
       sut.execute({
         name: 'Salon',
-        description: 'Best salon of the city',
+        description: 'Salon under registration...',
         phone: '55 555-5555',
         imageUrl: 'image.url',
-        email: 'barber@example.com',
+        email: 'registered_establishment@example.com', // invalid email
         password: '123456',
         latitude: -27.2092052,
         longitude: -49.6401091,
