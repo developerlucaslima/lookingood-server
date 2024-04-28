4744d212039a861387f48fcb349413c107760f87
developerlucaslima <developerlucaslima@gmail.com>
2024-04-28 17:19:29 -0300

    🧪 test: semantics improved and setup deleted

--- tests/Integration/get-user-profile.test.ts
+++ tests/Integration/get-user-profile.test.ts
@@ -2,17 +2,26 @@ import { expect, describe, it, beforeEach } from 'vitest'
 import { GetUserProfileUseCase } from '@/use-cases/get-user-profile'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { UserNotFoundException } from '@/use-cases/errors/404-user-not-found-exception'
-import { usersSetup } from 'tests/setup/users-setup'
+import { hash } from 'bcryptjs'
 
 let usersRepository: InMemoryUsersRepository
 let sut: GetUserProfileUseCase
 
 describe('Get User Profile Use Case', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     usersRepository = new InMemoryUsersRepository()
     sut = new GetUserProfileUseCase(usersRepository)
 
-    usersSetup(usersRepository)
+    const userId = 'User-01'
+    usersRepository.items.set(userId, {
+      id: userId,
+      name: 'Registered User',
+      serviceGender: 'BOTH',
+      email: 'registered_user@example.com',
+      passwordHash: await hash('123456', 6),
+      createdAt: new Date(),
+      role: 'USER',
+    })
   })
 
   it('should allow get user profile', async () => {
@@ -26,7 +35,7 @@ describe('Get User Profile Use Case', () => {
   it('it prevent get user profile if user does not exist', async () => {
     await expect(
       sut.execute({
-        userId: 'Nonexistent-User-01',
+        userId: 'Nonexistent-User-01', // invalid user
       }),
     ).rejects.toBeInstanceOf(UserNotFoundException)
   })
