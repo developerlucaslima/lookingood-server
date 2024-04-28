d61663a84b9db50bfb996deba6f5ad2bc77c7489
developerlucaslima <developerlucaslima@gmail.com>
2024-04-28 17:20:02 -0300

    🧪 test: semantics improved and setup deleted

--- tests/Integration/add-professional-schedules.test.ts
+++ tests/Integration/add-professional-schedules.test.ts
@@ -8,8 +8,9 @@ import { EstablishmentNotFoundException } from '@/use-cases/errors/404-establish
 import { ProfessionalNotFoundException } from '@/use-cases/errors/404-professional-not-found-exception'
 import { ScheduleNotFoundException } from '@/use-cases/errors/404-schedule-not-found-exception'
 import { InvalidEmployeeScheduleException } from '@/use-cases/errors/422-invalid-employee-schedule-exception'
-import { establishmentsSetup } from 'tests/setup/establishments-setup'
-import { professionalsSetup } from 'tests/setup/professionals-setup'
+import { WeekDay } from '@prisma/client'
+import { Decimal } from '@prisma/client/runtime/library'
+import { hash } from 'bcryptjs'
 import { beforeEach, it, expect, describe } from 'vitest'
 
 let professionalsRepository: InMemoryProfessionalsRepository
@@ -19,7 +20,7 @@ let establishmentSchedulesRepository: InMemoryEstablishmentsSchedulesRepository
 let sut: AddProfessionalScheduleUseCase
 
 describe('Add Professional Schedule Use Case', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     professionalsRepository = new InMemoryProfessionalsRepository()
     establishmentsRepository = new InMemoryEstablishmentsRepository()
     professionalSchedulesRepository =
@@ -34,8 +35,61 @@ describe('Add Professional Schedule Use Case', () => {
       establishmentSchedulesRepository,
     )
 
-    professionalsSetup(professionalsRepository)
-    establishmentsSetup(establishmentsRepository)
+    // Establishment 01 -------------------
+    const establishmentId = 'Establishment-01'
+    establishmentsRepository.items.set(establishmentId, {
+      id: establishmentId,
+      name: 'Registered Establishment',
+      description: 'Registered establishment...',
+      phone: '55 555-5555',
+      imageUrl: 'image.url',
+      email: 'registered_establishment@example.com',
+      passwordHash: await hash('123456', 6),
+      createdAt: new Date(),
+      latitude: new Decimal(-27.2092052),
+      longitude: new Decimal(-49.6401091),
+      role: 'ESTABLISHMENT',
+    })
+
+    // Establishment Schedule 01 -------------------
+    const establishmentSchedule = [
+      { weekDay: 'MONDAY' as WeekDay, id: 'Monday-Schedule' },
+      { weekDay: 'TUESDAY' as WeekDay, id: 'Tuesday-Schedule' },
+      { weekDay: 'WEDNESDAY' as WeekDay, id: 'Wednesday-Schedule' },
+      { weekDay: 'THURSDAY' as WeekDay, id: 'Thursday-Schedule' },
+      { weekDay: 'FRIDAY' as WeekDay, id: 'Friday-Schedule' },
+      { weekDay: 'SATURDAY' as WeekDay, id: 'Saturday-Schedule' },
+      // { weekDay: 'SUNDAY' as WeekDay, id: 'Sunday-Schedule' },
+    ]
+    establishmentSchedule.forEach(({ weekDay, id }) => {
+      establishmentSchedulesRepository.items.set(id, {
+        id,
+        startTime: '08:00',
+        minutesWorking: 600,
+        breakTime: '12:00',
+        minutesBreak: 60,
+        weekDay,
+        establishmentId,
+      })
+    })
+
+    // Professional 01 -------------------
+    const professionalId = 'Professional-01'
+    professionalsRepository.items.set(professionalId, {
+      id: professionalId,
+      name: 'Registered Professional',
+      imageUrl: 'image.url',
+      establishmentId: 'Establishment-01',
+    })
+
+    // Professional 02 -------------------
+    const professionalId02 = 'Professional-02'
+    professionalsRepository.items.set(professionalId02, {
+      id: professionalId02,
+      name: 'Registered Professional',
+      imageUrl: 'image.url',
+      establishmentId: 'Establishment-02',
+    })
   })
 
   it('should allow add a professional schedule', async () => {
@@ -43,7 +97,7 @@ describe('Add Professional Schedule Use Case', () => {
       startTime: '08:00',
       minutesWorking: 480,
       breakTime: '12:00',
-      minutesBreak: 90,
+      minutesBreak: 60,
       weekDay: 'MONDAY',
       professionalId: 'Professional-01',
     })
@@ -72,7 +126,7 @@ describe('Add Professional Schedule Use Case', () => {
         breakTime: '12:00',
         minutesBreak: 90,
         weekDay: 'MONDAY',
-        professionalId: 'Professional-03', // professional from invalid establishment
+        professionalId: 'Professional-02', // professional from invalid establishment
       }),
     ).rejects.toBeInstanceOf(EstablishmentNotFoundException)
   })
@@ -101,18 +155,5 @@ describe('Add Professional Schedule Use Case', () => {
         professionalId: 'Professional-01',
       }),
     ).rejects.toBeInstanceOf(ScheduleNotFoundException)
-  })
-
-  it('should prevent add professional schedule if the establishment does not have opening hours for the given weekday', async () => {
-    await expect(() =>
-      sut.execute({
-        startTime: '01:00', // invalid schedule
-        minutesWorking: 120,
-        breakTime: '02:00',
-        minutesBreak: 30,
-        weekDay: 'SUNDAY',
-        professionalId: 'Professional-01',
-      }),
-    ).rejects.toBeInstanceOf(InvalidEmployeeScheduleException)
   })
 })
