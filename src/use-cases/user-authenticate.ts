3f8406567261be38425ccb516ea6f04fb4cb8fc7
developerlucaslima <developerlucaslima@gmail.com>
2024-05-01 15:58:07 -0300

    🧪 test: use cases refactored and organized

--- src/use-cases/user-authenticate.ts
+++ src/use-cases/user-authenticate.ts
@@ -1,7 +1,7 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { compare } from 'bcryptjs'
 import { User } from '@prisma/client'
-import { InvalidCredentialsException } from './errors/401-invalid-credentials-exception'
+import { UnauthorizedCredentialsException } from './errors/401-unauthorized-credentials-exception'
 
 interface UserAuthenticateUseCaseRequest {
   email: string
@@ -19,19 +19,19 @@ export class UserAuthenticateUseCase {
     email,
     password,
   }: UserAuthenticateUseCaseRequest): Promise<UserAuthenticateUseCaseResponse> {
-    // It should prevent user authenticate with wrong email
+    // It should prevent user authenticate with wrong email.
     const user = await this.usersRepository.findByEmail(email)
     if (!user) {
-      throw new InvalidCredentialsException()
+      throw new UnauthorizedCredentialsException()
     }
 
-    // It should prevent user authenticate with wrong password
+    // It should prevent user authenticate with wrong password.
     const doesPasswordsMatch = await compare(password, user.passwordHash)
     if (!doesPasswordsMatch) {
-      throw new InvalidCredentialsException()
+      throw new UnauthorizedCredentialsException()
     }
 
-    // It should allow user authenticate
+    // It should allow user authenticate.
     return {
       user,
     }
