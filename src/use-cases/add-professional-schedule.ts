981ec63c11e880df24012e82eba2ab39e0e9dc6a
developerlucaslima <developerlucaslima@gmail.com>
2024-05-01 15:59:06 -0300

    🧪 test: use cases refactored and organized

--- src/use-cases/add-professional-schedule.ts
+++ src/use-cases/add-professional-schedule.ts
@@ -3,12 +3,11 @@ import { $Enums, ProfessionalSchedule } from '@prisma/client'
 import { EstablishmentsRepository } from '@/repositories/establishments-repository'
 import { compareSchedules } from '@/utils/compare-schedules'
 import { ProfessionalNotFoundException } from './errors/404-professional-not-found-exception'
-import { EstablishmentNotFoundException } from './errors/404-establishment-not-found-exception'
 import { InvalidInputParametersException } from './errors/400-invalid-input-parameters-exception'
-import { ScheduleNotFoundException } from './errors/404-schedule-not-found-exception'
-import { InvalidEmployeeScheduleException } from './errors/422-invalid-employee-schedule-exception'
 import { EstablishmentsSchedulesRepository } from '@/repositories/establishments-schedules-repository'
 import { ProfessionalsSchedulesRepository } from '@/repositories/professionals-schedules-repository'
+import { InvalidScheduleException } from './errors/422-invalid-schedule-exception'
+import { UnauthorizedEstablishmentException } from './errors/401-unauthorized-establishment-exception'
 
 interface AddProfessionalScheduleUseCaseRequest {
   startTime: string
@@ -39,37 +38,42 @@ export class AddProfessionalScheduleUseCase {
     weekDay,
     professionalId,
   }: AddProfessionalScheduleUseCaseRequest): Promise<AddProfessionalScheduleUseCaseResponse> {
-    // It should prevent add professional schedule if the professional does not exist
+    // It should prevent add professional schedule if the professional does not exist.
     const professional =
       await this.professionalsRepository.findById(professionalId)
     if (!professional) {
       throw new ProfessionalNotFoundException()
     }
 
-    // It should prevent add professional schedule if the establishment does not exist
+    // It should prevent add professional schedule if the establishment does not exist.
     const establishment = await this.establishmentsRepository.findById(
       professional.establishmentId,
     )
     if (!establishment) {
-      throw new EstablishmentNotFoundException()
+      throw new UnauthorizedEstablishmentException('unauthenticated')
     }
 
-    // It should prevent add professional schedule with negative time parameters
+    // It should prevent add professional schedule with break if it have not break start or end time.
+    if ((!breakTime && minutesBreak) || (breakTime && !minutesBreak)) {
+      throw new InvalidScheduleException('invalid_break')
+    }
+
+    // It should prevent add professional schedule with negative time parameters.
     if (minutesWorking < 0 || (breakTime && minutesBreak && minutesBreak < 0)) {
-      throw new InvalidInputParametersException('Minutes cannot be negative.')
+      throw new InvalidInputParametersException('negative')
     }
 
-    // It should prevent add professional schedule if the establishment does not have opening hours for the given weekday
+    // It should prevent add professional schedule if the establishment does not have opening hours for the given weekday.
     const establishmentSchedule =
       await this.establishmentSchedulesRepository.findByEstablishmentIdAndWeekDay(
         establishment.id,
         weekDay,
       )
     if (!establishmentSchedule) {
-      throw new ScheduleNotFoundException(weekDay)
+      throw new InvalidScheduleException('establishment_no_schedule')
     }
 
-    // It should prevent add professional schedule if the professional's schedule conflicts with the establishment's schedule
+    // It should prevent add professional schedule if the professional's schedule conflicts with the establishment's schedule.
     if (weekDay === establishmentSchedule.weekDay) {
       const isCompatibleSchedules = compareSchedules(
         startTime,
@@ -82,11 +86,11 @@ export class AddProfessionalScheduleUseCase {
         establishmentSchedule.minutesBreak,
       )
       if (!isCompatibleSchedules) {
-        throw new InvalidEmployeeScheduleException(weekDay)
+        throw new InvalidScheduleException('mismatched')
       }
     }
 
-    // It should allow add professional schedule
+    // It should allow add professional schedule.
     const schedule = await this.professionalSchedulesRepository.create({
       startTime,
       minutesWorking,
