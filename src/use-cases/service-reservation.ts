6a343325ddc5e1b9bf563182c17b1a9799310316
developerlucaslima <developerlucaslima@gmail.com>
2024-05-01 15:58:13 -0300

    🧪 test: use cases refactored and organized

--- src/use-cases/service-reservation.ts
+++ src/use-cases/service-reservation.ts
@@ -5,15 +5,15 @@ import { EstablishmentsRepository } from '@/repositories/establishments-reposito
 import { ProfessionalsRepository } from '@/repositories/professionals-repository'
 import { ServicesRepository } from '@/repositories/services-repository'
 import { getEndTimeByStartTime } from '@/utils/get-end-time-by-start-time'
-import { EstablishmentNotFoundException } from './errors/404-establishment-not-found-exception'
 import { ProfessionalNotFoundException } from './errors/404-professional-not-found-exception'
 import { ResourceNotFoundException } from './errors/404-resource-not-found-exception'
 import { ServiceNotFoundException } from './errors/404-service-not-found-exception'
-import { UserNotFoundException } from './errors/404-user-not-found-exception'
 import { TimetableNotAvailableException } from './errors/409-timetable-not-available-exception'
 import { isWithinProfessionalsSchedule } from '@/utils/is-within-with-professionals-schedule'
 import { getDayNameOfWeek } from '@/utils/get-day-name-of-week'
 import { ProfessionalsSchedulesRepository } from '@/repositories/professionals-schedules-repository'
+import { EstablishmentNotFoundException } from './errors/404-establishment-not-found-exception'
+import { UnauthorizedUserException } from './errors/401-unauthorized-user-exception'
 
 interface ServiceReservationUseCaseRequest {
   startTime: Date
@@ -44,56 +44,53 @@ export class ServiceReservationUseCase {
     professionalId,
     establishmentId,
   }: ServiceReservationUseCaseRequest): Promise<ServiceReservationsUseCaseResponse> {
-    // It should prevent service reservation if professional does not exist
+    // It should prevent service reservation if professional does not exist.
     const professional =
       await this.professionalsRepository.findById(professionalId)
     if (!professional) {
       throw new ProfessionalNotFoundException()
     }
 
-    // It should prevent service reservation if service does not exist
+    // It should prevent service reservation if service does not exist.
     const service = await this.servicesRepository.findById(serviceId)
     if (!service) {
       throw new ServiceNotFoundException()
     }
     const endTime = getEndTimeByStartTime(startTime, service.durationMinutes)
 
-    // It should prevent service reservation if user does not exist
+    // It should prevent service reservation if user does not exist.
     const user = await this.usersRepository.findById(userId)
     if (!user) {
-      throw new UserNotFoundException()
+      throw new UnauthorizedUserException('unauthenticated')
     }
 
-    // It should prevent service reservation if establishment does not exist
+    // It should prevent service reservation if establishment does not exist.
     const establishment =
       await this.establishmentsRepository.findById(establishmentId)
     if (!establishment) {
       throw new EstablishmentNotFoundException()
     }
 
-    // It should prevent service reservation if the establishment or professional does not match the service
+    // It should prevent service reservation if the establishment, professional and service does not match.
     if (
-      establishmentId !== service.establishmentId &&
-      establishmentId !== professional.establishmentId
+      establishmentId !== service.establishmentId ||
+      establishmentId !== professional.establishmentId ||
+      service.establishmentId !== professional.establishmentId
     ) {
-      throw new ResourceNotFoundException()
+      throw new ResourceNotFoundException('mismatched')
     }
 
-    // It should prevent service reservation if there are conflicts in the professional's schedule
+    // It should prevent service reservation if there are conflicts in the professional's schedule.
     const conflicts = await this.reservationsRepository.isReservationConflict(
       professionalId,
       startTime,
       endTime,
     )
     if (conflicts) {
-      throw new TimetableNotAvailableException(
-        startTime.toString(),
-        endTime.toString(),
-        "It's already been reserved.",
-      )
+      throw new TimetableNotAvailableException('conflict')
     }
 
-    // It should prevent service reservation if the professional does not have operating hours for the given time
+    // It should prevent service reservation if the professional does not have operating hours for the given time.
     const dayOfWeek = getDayNameOfWeek(startTime)
     const professionalSchedule =
       await this.professionalSchedulesRepository.findByProfessionalIdAndWeekDay(
@@ -101,22 +98,14 @@ export class ServiceReservationUseCase {
         dayOfWeek,
       )
     if (!professionalSchedule) {
-      throw new TimetableNotAvailableException(
-        undefined,
-        undefined,
-        `Selected professional does not work on ${dayOfWeek.toLowerCase()}.`,
-      )
+      throw new TimetableNotAvailableException('professional_day_off')
     } else if (
       !isWithinProfessionalsSchedule(professionalSchedule, startTime, endTime)
     ) {
-      throw new TimetableNotAvailableException(
-        undefined,
-        undefined,
-        `Timetable from ${startTime.toLocaleString()} to ${endTime.toLocaleString()} is outside of professional's working hours`,
-      )
+      throw new TimetableNotAvailableException('professional_no_working')
     }
 
-    // should allow service reservation
+    // should allow service reservation.
     const reservation = await this.reservationsRepository.create({
       startTime,
       endTime,
