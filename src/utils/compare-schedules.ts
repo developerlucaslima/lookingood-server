a84ff4dd4f7fb7d486e70c9fea6fc59275c1f5ac
developerlucaslima <developerlucaslima@gmail.com>
2024-05-01 15:31:03 -0300

    ✨ feat: util for compare schedules fixed up

--- src/utils/compare-schedules.ts
+++ src/utils/compare-schedules.ts
@@ -12,43 +12,17 @@ export function compareSchedules(
   establishmentBreakTime: string | null, // HH:mm
   establishmentMinutesBreak: number | null, // total minutes
 ): boolean {
-  console.log(
-    `Professional: 
-    start = ${professionalStartTime}, 
-    minutesWorking = ${professionalMinutesWorking}, 
-    breakTime = ${professionalBreakTime}, 
-    minutesBreak = ${professionalMinutesBreak}`,
-  )
-
-  console.log(
-    `Establishment: 
-    start = ${establishmentStartMinutes}, 
-    minutesWorking = ${establishmentMinutesWorking}, 
-    breakTime = ${establishmentBreakTime}, 
-    minutesBreak = ${establishmentMinutesBreak}`,
-  )
-
   const professionalStart = getMinutesByTime(professionalStartTime)
   const professionalEnd = professionalStart + professionalMinutesWorking
 
   const establishmentStart = getMinutesByTime(establishmentStartMinutes)
   const establishmentEnd = establishmentStart + establishmentMinutesWorking
 
-  console.log(
-    `Professional: start = ${professionalStart}, end = ${professionalEnd}`,
-  )
-  console.log(
-    `Establishment: start = ${establishmentStart}, end = ${establishmentEnd}`,
-  )
-
   // Check if professional's working hours are within establishment's working hours
   if (
     professionalStart < establishmentStart ||
     professionalEnd > establishmentEnd
   ) {
-    console.log(
-      'Professional working hours are outside of establishment working hours',
-    )
     return false // Professional's working hours are outside of establishment's working hours
   }
 
@@ -66,13 +40,6 @@ export function compareSchedules(
     const establishmentBreakStart = getMinutesByTime(establishmentBreakTime)
     const establishmentBreakEnd =
       establishmentBreakStart + establishmentMinutesBreak
-
-    console.log(
-      `Professional: break start = ${professionalBreakStart}, end = ${professionalBreakEnd}`,
-    )
-    console.log(
-      `Establishment: break start = ${establishmentBreakStart}, end = ${establishmentBreakEnd}`,
-    )
 
     const breaksDiffer = !(
       professionalBreakStart === establishmentBreakStart &&
@@ -94,7 +61,6 @@ export function compareSchedules(
         endsWithinEstablishmentBreak ||
         spansEstablishmentBreak)
     ) {
-      console.log('Break times overlap')
       return false // Break times overlap
     }
   }
