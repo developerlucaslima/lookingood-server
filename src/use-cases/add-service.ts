a1a06dc4af5f25cfb472bdb9ec6fe0271d05830a
developerlucaslima <developerlucaslima@gmail.com>
2024-05-01 15:58:56 -0300

    🧪 test: use cases refactored and organized

--- src/use-cases/add-service.ts
+++ src/use-cases/add-service.ts
@@ -1,10 +1,10 @@
 import { ServicesRepository } from '@/repositories/services-repository'
 import { $Enums, Service } from '@prisma/client'
 import { EstablishmentsRepository } from '@/repositories/establishments-repository'
-import { EstablishmentNotFoundException } from './errors/404-establishment-not-found-exception'
 import { InvalidServiceDurationException } from './errors/422-invalid-service-duration-exception'
 import { InvalidGenderException } from './errors/422-invalid-gender-exception'
 import { isValidServiceDuration } from '@/utils/is-valid-service-duration'
+import { UnauthorizedEstablishmentException } from './errors/401-unauthorized-establishment-exception'
 
 interface AddServiceUseCaseRequest {
   name: string
@@ -37,24 +37,24 @@ export class AddServiceUseCase {
     establishmentId,
     durationMinutes,
   }: AddServiceUseCaseRequest): Promise<AddServiceUseCaseResponse> {
-    // It should prevent add service if the establishment does not exist
+    // It should prevent add service if the establishment does not exist.
     const establishment =
       await this.establishmentRepository.findById(establishmentId)
     if (!establishment) {
-      throw new EstablishmentNotFoundException()
+      throw new UnauthorizedEstablishmentException('unauthenticated')
     }
 
-    // It should prevent adding a service if the duration is not a multiple of 15 minutes
+    // It should prevent adding a service if the duration is not a multiple of 15 minutes.
     if (!isValidServiceDuration(durationMinutes)) {
       throw new InvalidServiceDurationException()
     }
 
-    // It should prevent add service if gender is not valid
+    // It should prevent add service if gender is not valid.
     if (!['MALE', 'FEMALE', 'BOTH'].includes(genderFor)) {
       throw new InvalidGenderException()
     }
 
-    // It should allow add service
+    // It should allow add service.
     const service = await this.servicesRepository.create({
       name,
       price,
