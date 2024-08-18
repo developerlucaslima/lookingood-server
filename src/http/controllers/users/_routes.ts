5be250dac7dfb67a476987f397df98f2de8ab486
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 15:11:37 -0300

    ✨routes: users

---
+++ src/http/controllers/users/_routes.ts
@@ -0,0 +1,24 @@
+import { FastifyInstance } from 'fastify'
+
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
+
+import { userAuthenticateController } from './user-authenticate'
+import { userProfileController } from './user-profile'
+import { userRefreshController } from './user-refresh'
+import { userRegisterController } from './user-register'
+
+export async function appUserRoutes(app: FastifyInstance) {
+  app.post('/users-register', userRegisterController)
+  app.post('/users-auth', userAuthenticateController)
+
+  /** Token Refresh */
+  app.patch('/token/user-refresh', userRefreshController)
+
+  /** Authenticated */
+  app.get(
+    '/me',
+    { onRequest: [verifyJwt, verifyUserRole('USER')] },
+    userProfileController,
+  )
+}
