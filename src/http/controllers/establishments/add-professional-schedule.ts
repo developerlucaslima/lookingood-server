54e79abfdbbc236052544ef414d72e03f48464c2
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 15:12:10 -0300

    ✨ controller: add professional schedule

--- src/http/controllers/establishments/add-professional-schedule.ts
+++ src/http/controllers/establishments/add-professional-schedule.ts
@@ -1,21 +1,31 @@
-import { ProfessionalNotFoundError } from '@/use-cases/errors/professional-not-found-error'
-import { makeAddProfessionalScheduleUseCase } from '@/use-cases/factories/make-add-professional-schedule-use-case'
-import { FastifyRequest, FastifyReply } from 'fastify'
+import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
-export async function addProfessionalSchedule(
+import { ProfessionalNotFoundException } from '@/errors/professional-not-found.exception'
+import { makeAddProfessionalScheduleUseCase } from '@/use-cases/factories/make-add-professional-schedule-use-case'
+
+export async function addProfessionalScheduleController(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
   const timeRegex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/
   const invalidTime = 'Invalid time'
 
-  const { startTime, minutesWorking, breakTime, minutesBreak } = z
+  const { startTime, minutesWorking, breakTime, minutesBreak, weekDay } = z
     .object({
       startTime: z.string().regex(timeRegex, { message: invalidTime }),
       minutesWorking: z.number().min(2).max(4),
       breakTime: z.string().regex(timeRegex, { message: invalidTime }),
       minutesBreak: z.number().min(2).max(3),
+      weekDay: z.enum([
+        'MONDAY',
+        'TUESDAY',
+        'WEDNESDAY',
+        'THURSDAY',
+        'FRIDAY',
+        'SATURDAY',
+        'SUNDAY',
+      ]),
     })
     .parse(request.body)
 
@@ -27,10 +37,11 @@ export async function addProfessionalSchedule(
       minutesWorking,
       breakTime,
       minutesBreak,
+      weekDay,
       professionalId: request.user.sub,
     })
   } catch (err) {
-    if (err instanceof ProfessionalNotFoundError) {
+    if (err instanceof ProfessionalNotFoundException) {
       return reply.status(404).send({ message: err.message })
     }
 
