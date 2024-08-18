fe93e66730fa1bc44aae8df06cdb0d8ee35d40c3
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 14:49:22 -0300

    🥅 errors: professional not found

---
+++ src/errors/professional-not-found.exception.ts
@@ -0,0 +1,11 @@
+const NOT_FOUND_ERROR_CODE = 404
+
+export class ProfessionalNotFoundException extends Error {
+  public readonly code: number
+
+  constructor() {
+    super('Oops! Professional not found.')
+    this.name = 'ProfessionalNotFoundException'
+    this.code = NOT_FOUND_ERROR_CODE
+  }
+}
