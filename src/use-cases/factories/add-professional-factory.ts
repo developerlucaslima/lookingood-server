f004f2cb60db179539a10d236a4c4bafd25a30cb
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 16:45:45 -0300

    ✨ factory: add professional

---
+++ src/use-cases/factories/add-professional-factory.ts
@@ -0,0 +1,16 @@
+import { PrismaEstablishmentsRepository } from '@/repositories/prisma/prisma-establishments-repository'
+import { PrismaProfessionalsRepository } from '@/repositories/prisma/prisma-professionals-repository'
+
+import { AddProfessionalUseCase } from '../add-professional'
+
+export function addProfessionalFactory() {
+  const establishmentsRepository = new PrismaEstablishmentsRepository()
+  const professionalsRepository = new PrismaProfessionalsRepository()
+
+  const addProfessionalUseCase = new AddProfessionalUseCase(
+    establishmentsRepository,
+    professionalsRepository,
+  )
+
+  return addProfessionalUseCase
+}
