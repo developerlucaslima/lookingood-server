8cfc67a6c0c0c1898e45596ca77f602e2508c52f
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 14:34:51 -0300

    🥅 errors: invalid time format

---
+++ src/errors/invalid-time-format.exception.ts
@@ -0,0 +1,11 @@
+const UNPROCESSABLE_ENTITY_ERROR_CODE = 422
+
+export class InvalidTimeFormatException extends Error {
+  public readonly code: number
+
+  constructor() {
+    super('Oops! Invalid time format.')
+    this.name = 'InvalidTimeFormatException'
+    this.code = UNPROCESSABLE_ENTITY_ERROR_CODE
+  }
+}
