cff52b3be7671174bf9a0ca7984fa885b7bc3ed9
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 13:39:34 -0300

    🗑️ remove: unused files

--- prisma/migrations/20240424222102_reservation_status/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `status` column on the `reservations` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "reservations" DROP COLUMN "status",
-ADD COLUMN     "status" "Status" NOT NULL DEFAULT 'WAITING_FOR_CONFIRMATION';
