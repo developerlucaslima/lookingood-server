2a3632b8fb8a169929cad71718f89c60ff808d5e
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 16:47:49 -0300

    ✨ factory: update booking service

---
+++ src/use-cases/factories/update-booking-service-factory.ts
@@ -0,0 +1,29 @@
+import { PrismaBookingsRepository } from '@/repositories/prisma/prisma-bookings-repository'
+import { PrismaEstablishmentsRepository } from '@/repositories/prisma/prisma-establishments-repository'
+import { PrismaProfessionalsRepository } from '@/repositories/prisma/prisma-professionals-repository'
+import { PrismaProfessionalsSchedulesRepository } from '@/repositories/prisma/prisma-professionals-schedule-repository'
+import { PrismaServicesRepository } from '@/repositories/prisma/prisma-services-repository'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+
+import { UpdateBookingServiceUseCase } from '../update-booking-service'
+
+export function updateBookingServiceFactory() {
+  const establishmentsRepository = new PrismaEstablishmentsRepository()
+  const professionalsRepository = new PrismaProfessionalsRepository()
+  const servicesRepository = new PrismaServicesRepository()
+  const professionalSchedulesRepository =
+    new PrismaProfessionalsSchedulesRepository()
+  const reservationsRepository = new PrismaBookingsRepository()
+  const usersRepository = new PrismaUsersRepository()
+
+  const serviceBookingUpdateUseCase = new UpdateBookingServiceUseCase(
+    establishmentsRepository,
+    professionalsRepository,
+    professionalSchedulesRepository,
+    servicesRepository,
+    reservationsRepository,
+    usersRepository,
+  )
+
+  return serviceBookingUpdateUseCase
+}
