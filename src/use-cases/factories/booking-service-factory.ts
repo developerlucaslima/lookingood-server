007a78198217381b58c80fe66e924c0cb4ea4d35
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 16:47:00 -0300

    ✨ factory: booking service

---
+++ src/use-cases/factories/booking-service-factory.ts
@@ -0,0 +1,29 @@
+import { PrismaBookingsRepository } from '@/repositories/prisma/prisma-bookings-repository'
+import { PrismaEstablishmentsRepository } from '@/repositories/prisma/prisma-establishments-repository'
+import { PrismaProfessionalsRepository } from '@/repositories/prisma/prisma-professionals-repository'
+import { PrismaProfessionalsSchedulesRepository } from '@/repositories/prisma/prisma-professionals-schedule-repository'
+import { PrismaServicesRepository } from '@/repositories/prisma/prisma-services-repository'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+
+import { BookingServiceUseCase } from '../booking-service'
+
+export function bookingServiceFactory() {
+  const establishmentsRepository = new PrismaEstablishmentsRepository()
+  const professionalsRepository = new PrismaProfessionalsRepository()
+  const professionalSchedulesRepository =
+    new PrismaProfessionalsSchedulesRepository()
+  const servicesRepository = new PrismaServicesRepository()
+  const reservationsRepository = new PrismaBookingsRepository()
+  const usersRepository = new PrismaUsersRepository()
+
+  const serviceBookingUseCase = new BookingServiceUseCase(
+    establishmentsRepository,
+    professionalsRepository,
+    professionalSchedulesRepository,
+    servicesRepository,
+    reservationsRepository,
+    usersRepository,
+  )
+
+  return serviceBookingUseCase
+}
