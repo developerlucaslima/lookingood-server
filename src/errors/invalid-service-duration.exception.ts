0e7cc3f74f2e2708e6df4f61564dc00ca344051a
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 14:34:44 -0300

    🥅 errors: invalid service duration

---
+++ src/errors/invalid-service-duration.exception.ts
@@ -0,0 +1,11 @@
+const UNPROCESSABLE_ENTITY_ERROR_CODE = 422
+
+export class InvalidServiceDurationException extends Error {
+  public readonly code: number
+
+  constructor() {
+    super('Oops! Invalid service duration.')
+    this.name = 'InvalidServiceDurationException'
+    this.code = UNPROCESSABLE_ENTITY_ERROR_CODE
+  }
+}
