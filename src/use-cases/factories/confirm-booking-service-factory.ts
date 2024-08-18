eb8ee54fb495c1bcebec802185de7233ed2d197e
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 16:47:07 -0300

    ✨ factory: confirm booking service

---
+++ src/use-cases/factories/confirm-booking-service-factory.ts
@@ -0,0 +1,15 @@
+import { PrismaBookingsRepository } from '@/repositories/prisma/prisma-bookings-repository'
+import { PrismaEstablishmentsRepository } from '@/repositories/prisma/prisma-establishments-repository'
+
+import { ConfirmBookingServiceUseCase } from '../confirm-booking-service'
+
+export function confirmBookingServiceFactory() {
+  const reservationsRepository = new PrismaBookingsRepository()
+  const establishmentsRepository = new PrismaEstablishmentsRepository()
+  const confirmBookingServiceUseCase = new ConfirmBookingServiceUseCase(
+    reservationsRepository,
+    establishmentsRepository,
+  )
+
+  return confirmBookingServiceUseCase
+}
