f3e5fa6e9a7e0346c60c6d8091ab4087b116eae1
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 16:46:43 -0300

    ✨ factory: add service schedule

---
+++ src/use-cases/factories/add-service-factory.ts
@@ -0,0 +1,16 @@
+import { PrismaEstablishmentsRepository } from '@/repositories/prisma/prisma-establishments-repository'
+import { PrismaServicesRepository } from '@/repositories/prisma/prisma-services-repository'
+
+import { AddServiceUseCase } from '../add-service'
+
+export function addServiceFactory() {
+  const establishmentsRepository = new PrismaEstablishmentsRepository()
+  const servicesRepository = new PrismaServicesRepository()
+
+  const addServiceUseCase = new AddServiceUseCase(
+    establishmentsRepository,
+    servicesRepository,
+  )
+
+  return addServiceUseCase
+}
