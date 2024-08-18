48e58723b15b0a91353cce625d2af8619b8f709c
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 16:42:29 -0300

    🗑️ remove: unused files

--- src/use-cases/factories/make-user-authenticate-use-case.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { UserAuthenticateUseCase } from '../user-authenticate'
-
-export function makeUserAuthenticateUseCase() {
-  const usersRepository = new PrismaUsersRepository()
-  const userAuthenticateUseCase = new UserAuthenticateUseCase(usersRepository)
-
-  return userAuthenticateUseCase
-}
--- src/use-cases/factories/make-user-register-use-case.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { UserRegisterUseCase } from '../user-register'
-
-export function makeUserRegisterUseCase() {
-  const usersRepository = new PrismaUsersRepository()
-  const userRegisterUseCase = new UserRegisterUseCase(usersRepository)
-
-  return userRegisterUseCase
-}
