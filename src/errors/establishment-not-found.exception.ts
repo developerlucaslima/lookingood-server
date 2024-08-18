e298ab3f295f78398fc8d447021c5e8b763b7ca3
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 14:33:46 -0300

    🥅 errors: established not found

---
+++ src/errors/establishment-not-found.exception.ts
@@ -0,0 +1,11 @@
+const NOT_FOUND_ERROR_CODE = 404
+
+export class EstablishmentNotFoundException extends Error {
+  public readonly code: number
+
+  constructor() {
+    super('Oops! Establishment not found.')
+    this.name = 'EstablishmentNotFoundException'
+    this.code = NOT_FOUND_ERROR_CODE
+  }
+}
