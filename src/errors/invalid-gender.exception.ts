5f0d237c48dac3261776d388ddfcb7e78a1b2740
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 14:34:06 -0300

    🥅 errors: invalid gender

---
+++ src/errors/invalid-gender.exception.ts
@@ -0,0 +1,11 @@
+const UNPROCESSABLE_ENTITY_ERROR_CODE = 422
+
+export class InvalidGenderException extends Error {
+  public readonly code: number
+
+  constructor() {
+    super('Oops! Invalid gender.')
+    this.name = 'InvalidGenderException'
+    this.code = UNPROCESSABLE_ENTITY_ERROR_CODE
+  }
+}
