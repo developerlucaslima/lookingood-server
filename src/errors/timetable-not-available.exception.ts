f3d8a5daf2131da4e88b2c948dc5e7554abf5ad8
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 14:49:40 -0300

    🥅 errors: timetable not available

---
+++ src/errors/timetable-not-available.exception.ts
@@ -0,0 +1,11 @@
+const CONFLICT_ERROR_CODE = 409
+
+export class TimetableNotAvailableException extends Error {
+  public readonly code: number
+
+  constructor() {
+    super('Oops! Timetable not available.')
+    this.name = 'TimetableNotAvailableException'
+    this.code = CONFLICT_ERROR_CODE
+  }
+}
