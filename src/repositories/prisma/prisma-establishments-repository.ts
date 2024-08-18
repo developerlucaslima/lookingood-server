3c0326a454d35d1db8385c64f690431792b964e0
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 15:36:00 -0300

    ✨ prisma repository: establishments

--- src/repositories/prisma/prisma-establishments-repository.ts
+++ src/repositories/prisma/prisma-establishments-repository.ts
@@ -1,5 +1,7 @@
 import { Prisma } from '@prisma/client'
+
 import { prisma } from '@/prisma'
+
 import { EstablishmentsRepository } from '../establishments-repository'
 
 export class PrismaEstablishmentsRepository
