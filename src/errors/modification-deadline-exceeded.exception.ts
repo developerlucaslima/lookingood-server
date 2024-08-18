7e1f53f68d111571bb98617705ba0fe79d016137
devlucaslima <developerlucaslima@gmail.com>
2024-08-18 14:48:46 -0300

    🥅 errors: modification deadline exceeded

---
+++ src/errors/modification-deadline-exceeded.exception.ts
@@ -0,0 +1,11 @@
+const UNPROCESSABLE_ENTITY_ERROR_CODE = 422
+
+export class ModificationDeadlineExceededException extends Error {
+  public readonly code: number
+
+  constructor() {
+    super('Oops! Modification deadline exceeded.')
+    this.name = 'ModificationDeadlineExceededException'
+    this.code = UNPROCESSABLE_ENTITY_ERROR_CODE
+  }
+}
