c5097d93815e10555e6bd7459e8e2ceb44a5b428
developerlucaslima <developerlucaslima@gmail.com>
2024-09-17 12:06:57 -0300

    :broom: lint: from eslint and prettier to biome

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -3,40 +3,38 @@ import 'dotenv/config'
 import { execSync } from 'node:child_process'
 
 import { randomUUID } from 'node:crypto'
-import { Environment } from 'vitest'
+import type { Environment } from 'vitest'
 
 const prisma = new PrismaClient()
 
-export default <Environment>{
-  name: 'prisma',
-  transformMode: 'ssr',
-  async setup() {
-    const randomSchema = randomUUID()
-    const databaseURL = setSchemaDatabaseURL(randomSchema)
+export default (<Environment>{
+	name: 'prisma',
+	transformMode: 'ssr',
+	async setup() {
+		const randomSchema = randomUUID()
+		const databaseURL = setSchemaDatabaseURL(randomSchema)
 
-    process.env.DATABASE_URL = databaseURL
+		process.env.DATABASE_URL = databaseURL
 
-    execSync('npx prisma migrate deploy')
+		execSync('npx prisma migrate deploy')
 
-    return {
-      async teardown() {
-        await prisma.$executeRawUnsafe(
-          `DROP SCHEMA IF EXISTS "${randomSchema}" CASCADE`,
-        )
+		return {
+			async teardown() {
+				await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${randomSchema}" CASCADE`)
 
-        await prisma.$disconnect()
-      },
-    }
-  },
-}
+				await prisma.$disconnect()
+			},
+		}
+	},
+})
 
 function setSchemaDatabaseURL(schema: string) {
-  if (!process.env.DATABASE_URL) {
-    throw new Error('Please specify a DATABASE_URL environment variable.')
-  }
+	if (!process.env.DATABASE_URL) {
+		throw new Error('Please specify a DATABASE_URL environment variable.')
+	}
 
-  const url = new URL(process.env.DATABASE_URL)
-  url.searchParams.set('schema', schema)
+	const url = new URL(process.env.DATABASE_URL)
+	url.searchParams.set('schema', schema)
 
-  return url.toString()
+	return url.toString()
 }
